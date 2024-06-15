95c19f1e0ee7f1586959ee4d5831bc21d29c518d
michav95 <msc5689@yahoo.com>
2024-06-15 11:35:51 -0500

    Upload files

---
+++ app.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import streamlit as st
+data = pd.DataFrame({
+"col1": [1, 2, 3],
+"col2": [4, 5, 6],
+})
+st.write(data)
+st.header('Tossing a Coin')
+
+st.write('It is not a functional application yet. Under construction.')
